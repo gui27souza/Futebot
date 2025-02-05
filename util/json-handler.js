fc3ea23ff804a735b2e2c5135985ea8da950fb1e
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-02-05 16:19:21 -0300

    json handler

---
+++ util/json-handler.js
@@ -0,0 +1,61 @@
+// Module imports
+const fs = require('fs')
+const path = require('path')
+
+// Generic function to read json data files
+    
+    function readJsonFile(fileName) {
+
+        const dataPath = path.join(__dirname, `../data/${fileName}`)
+
+        if (!fs.existsSync(dataPath)) {
+            console.error(`Arquivo ${fileName} não encontrado em:`, dataPath)
+            process.exit(1)
+        }
+
+        const data = fs.readFileSync(dataPath, 'utf8')
+        return JSON.parse(data)
+    }
+
+// 
+
+// Read the data files and settings file
+    
+    function readConfig() {
+        return readJsonFile('config.json')
+    }
+
+    function readLista() {
+        return readJsonFile('lista.json')
+    }
+// 
+
+// Generic function to update JSON files with new data
+
+    function updateJsonFile(fileName, updatedDataJson) {
+
+        const dataPath = path.join(__dirname, `../data/${fileName}`)
+
+        const updatedData = JSON.stringify(updatedDataJson, null, 3)
+
+        fs.writeFileSync(dataPath, updatedData, 'utf8')
+
+        return true
+    }
+
+// 
+
+// Update the album library data and the settings
+    
+    function updateConfig(updatedConfigJson) {
+        return updateJsonFile('config.json', updatedConfigJson)
+    }
+    
+    function updateLista(updatedListaJson) {
+        return updateJsonFile('lista.json', updatedListaJson)
+    }
+
+// 
+
+// Module exports
+module.exports = { readConfig, updateConfig, readLista, updateLista }
